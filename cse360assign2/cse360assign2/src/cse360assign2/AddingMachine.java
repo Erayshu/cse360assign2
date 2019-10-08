8581183d66e0627dd531c233192ddf607170c122
Vince <Vince@DESKTOP-LT69NKQ>
2019-10-08 15:45:33 -0700

    Completed Adding Machine

--- cse360assign2/cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,60 +1,72 @@
+//***********************************
+//Name: Vincent Le
+//Class: CSE 360
+//Assignment: cse360assign2
+//Description: Simple addition and subtraction machine that will return
+//			calculated value and equation generated.
+//***********************************
+
+
 package cse360assign2;
 
 /**
  * 
  * @author Vincent Le
- *
+ * @param total (int) Total number in adding machine.
+ * @param history (String) Equation built by commands used in adding machine.
  */
 public class AddingMachine {
 
-	/**
-	 * 
-	 */
 	private int total;
+	private String history;
 	
 	/**
-	 * 
+	 * Constructor initializing total.
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Method to return total in adding machine.
+	 * @return Total number in adding machine.
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
-	 * 
-	 * @param value
+	 * Adds value to total.
+	 * @param value Inputed integer to be added to total.
 	 */
 	public void add (int value) {
-		
+		total = total + value;
+		history = history + " + " + value;
 	}
 	
 	/**
-	 * 
-	 * @param value
+	 * Subtracts value from total.
+	 * @param value Inputed integer to be subtracted from total.
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
+		history = history + " - " + value;
 	}
 		
 	/**
-	 * 
-	 * @return
+	 * Method to return history of commands as an equation.
+	 * @return History of all commands.
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	/**
-	 * 
+	 * Method to erase history and reset adding machine.
 	 */
 	public void clear() {
-	
+		total = 0;
+		history = "0";
 	}
 }
